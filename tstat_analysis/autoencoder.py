f09a076b9143c3bbbf92f49c290cea4a105eed02
Mariam Kiran <mariam.kiran@gmail.com>
2018-08-10 22:15:58 -0700

    new visualization

--- tstat_analysis/autoencoder.py
+++ tstat_analysis/autoencoder.py
@@ -1,4 +1,5 @@
 #autoencoder.py
+from __future__ import print_function
 
 from pandas import read_csv, DataFrame
 from numpy.random import seed
@@ -8,8 +9,8 @@ from keras.layers import Input, Dense
 from keras.models import Model
 import os
 import pandas as pd
+import numpy as np
 from sklearn.preprocessing import StandardScaler
-
 
 #----
 
@@ -18,7 +19,7 @@ from sklearn.preprocessing import StandardScaler
 #raw_data={'Average rtt C2S', 'Average rtt S2C','target'}
 #df=pd.DataFrame(raw_data, columns = ['Sent','Received','Lost','Duplicated','Reordered'])
 
-label_file = os.path.join("1000Genomelogs_labelleddata/fullfeature-26variables.csv")
+label_file = os.path.join("1000Genomelogs_labelleddata/full26features.csv")
 
 df=pd.read_csv(label_file)
 
@@ -50,7 +51,8 @@ x = StandardScaler().fit_transform(x)
 
 print("1")
 
-targets = ['Normal', 'Loss1%','Loss5%', 'pDup1%', 'pDup5%','reord25-50%','reord50-50%', 'Loss3%']
+targets = ['Normal', 'Loss1%', 'Loss2%', 'pDup1%', 'pDup5%', 'reord25-50%','reord50-50%', 'Loss3%']
+#'NoFlow', 'Loss1%','Loss5%', 'pDup1%', 'pDup5%','reord25-50%','reord50-50%']
   
 
 colors = ['r', 'g', 'b', 'black', 'lime', 'yellow', 'cyan', 'coral']
@@ -60,7 +62,7 @@ colors = ['r', 'g', 'b', 'black', 'lime', 'yellow', 'cyan', 'coral']
 # SCALE EACH FEATURE INTO [0, 1] RANGE
 sX = minmax_scale(x, axis = 0)
 ncol = sX.shape[1]
-X_train, X_test, Y_train, Y_test = train_test_split(sX, y, train_size = 0.5, random_state = seed(2017))
+X_train, X_test, Y_train, Y_test = train_test_split(sX, y, train_size = 0.1, random_state = seed(2017))
  
 ### AN EXAMPLE OF SIMPLE AUTOENCODER ###
 # InputLayer (None, 10)
@@ -73,25 +75,82 @@ print("test")
 print(X_test)
 
 input_dim = Input(shape = (ncol, ))
-# DEFINE THE DIMENSION OF ENCODER ASSUMED 3
-encoding_dim = 3
-# DEFINE THE ENCODER LAYER
+# # DEFINE THE DIMENSION OF ENCODER ASSUMED 3
+encoding_dim = 2
+# # DEFINE THE ENCODER LAYER
 encoded = Dense(encoding_dim, activation = 'relu')(input_dim)
-# DEFINE THE DECODER LAYER
+# # DEFINE THE DECODER LAYER
 decoded = Dense(ncol, activation = 'sigmoid')(encoded)
-# COMBINE ENCODER AND DECODER INTO AN AUTOENCODER MODEL
+# # COMBINE ENCODER AND DECODER INTO AN AUTOENCODER MODEL
 autoencoder = Model(input = input_dim, output = decoded)
-# CONFIGURE AND TRAIN THE AUTOENCODER
+# # CONFIGURE AND TRAIN THE AUTOENCODER
 autoencoder.compile(optimizer = 'adadelta', loss = 'binary_crossentropy')
 autoencoder.fit(X_train, X_train, nb_epoch = 50, batch_size = 100, shuffle = True, validation_data = (X_test, X_test))
 
-# THE ENCODER TO EXTRACT THE REDUCED DIMENSION FROM THE ABOVE AUTOENCODER
+# # THE ENCODER TO EXTRACT THE REDUCED DIMENSION FROM THE ABOVE AUTOENCODER
 encoder = Model(inputs = input_dim, outputs = encoded)
 encoded_input = Input(shape = (encoding_dim, ))
 encoded_out = encoder.predict(X_test)
 encoded_out[0:2]
 
-print(encoded_out)
+# cellvalue='abc'
+
+pd.set_option('display.max_columns', None)
+
+log = open("resultsc.csv", "w")
+for x in encoded_out:
+	cellvalue = np.array2string(x, precision=4, separator=',',suppress_small=True)
+ 	cellvalue = cellvalue.replace("[", "")
+ 	cellvalue = cellvalue.replace("]", "")
+ 	print(cellvalue, file = log)
+
+print(encoded_out) #print into a csv file
+
+# #for x in encoded_out:
+# 	#print x
+
+print(encoded_out.size)
 #array([[ 0.        ,  1.26510417,  1.62803197],
+#       [ 2.32508397,  0.99735016,  2.06461048]], dtype=float32)
+######
+
+# input_dim = Input(shape = (ncol, ))
+# # DEFINE THE DIMENSION OF ENCODER ASSUMED 3
+# encoding_dim = 2
+# # DEFINE THE ENCODER LAYERS
+# encoded1 = Dense(20, activation = 'relu')(input_dim)
+# encoded2 = Dense(10, activation = 'relu')(encoded1)
+# encoded3 = Dense(5, activation = 'relu')(encoded2)
+# encoded4 = Dense(encoding_dim, activation = 'relu')(encoded3)
+# # DEFINE THE DECODER LAYERS
+# decoded1 = Dense(5, activation = 'relu')(encoded4)
+# decoded2 = Dense(10, activation = 'relu')(decoded1)
+# decoded3 = Dense(20, activation = 'relu')(decoded2)
+# decoded4 = Dense(ncol, activation = 'sigmoid')(decoded3)
+# # COMBINE ENCODER AND DECODER INTO AN AUTOENCODER MODEL
+# autoencoder = Model(input = input_dim, output = decoded4)
+# # CONFIGURE AND TRAIN THE AUTOENCODER
+# autoencoder.compile(optimizer = 'adadelta', loss = 'binary_crossentropy')
+# autoencoder.fit(X_train, X_train, nb_epoch = 100, batch_size = 100, shuffle = True, validation_data = (X_test, X_test))
+# # THE ENCODER TO EXTRACT THE REDUCED DIMENSION FROM THE ABOVE AUTOENCODER
+# encoder = Model(input = input_dim, output = encoded4)
+# encoded_input = Input(shape = (encoding_dim, ))
+# encoded_out = encoder.predict(X_test)
+# encoded_out[0:2]
+
+# log = open("results2.csv", "w")
+# for x in encoded_out:
+# 	cellvalue = np.array2string(x, precision=4, separator=',',suppress_small=True)
+# 	cellvalue = cellvalue.replace("[", "")
+# 	cellvalue = cellvalue.replace("]", "")
+# 	print(cellvalue, file = log)
+
+# print(encoded_out) #print into a csv file
+
+# # #for x in encoded_out:
+# # 	#print x
+
+# print(encoded_out.size)
+# #array([[ 0.        ,  1.26510417,  1.62803197],
 #       [ 2.32508397,  0.99735016,  2.06461048]], dtype=float32)
 
--- tstat_analysis/controldata/isolationfolder/plotData.py
+++ tstat_analysis/controldata/isolationfolder/plotData.py
@@ -1,11 +1,16 @@
-#test
-#PCA for paper
+#plotData
+
+#PCA_FullTstat.py
+
 
 from sklearn import cluster, datasets
+from mpl_toolkits.mplot3d import Axes3D
 import matplotlib.pyplot as plt
 import numpy as np
 import pandas as pd 
 import os
+from sklearn import preprocessing
+
 
 
 from sklearn.preprocessing import StandardScaler
@@ -13,7 +18,12 @@ from sklearn.preprocessing import StandardScaler
 from sklearn.decomposition import PCA, IncrementalPCA
 
 
-label_file = os.path.join("1000Genomelogs_labelleddata/full26features.csv")
+label_file = os.path.join("../resultsc.csv")
+
+
+
+#results2.csv")
+#cleanDataPCATest.csv")
 
 #raw_data={'Average rtt C2S', 'Average rtt S2C','target'}
 #df=pd.DataFrame(raw_data, columns = ['Sent','Received','Lost','Duplicated','Reordered'])
@@ -22,29 +32,44 @@ df=pd.read_csv(label_file)
 
 
 # load dataset into Pandas DataFrame
-df = pd.read_csv(label_file, names=['Average rtt C2S', 'Average rtt S2C','max seg size1','min seg size1',
-	'win max1','win min1','win zero1','cwin max1','cwin min1','initial cwin1','rtx RTO1','rtx FR1','reordering1','net dup1',
-	'max seg size','in seg size','win max','win min','win zero','cwin max','cwin min','initial cwin','rtx RTO','rtx FR','eordering',
-	'net dup','target'])
+#df = pd.read_csv(label_file, names=['Average rtt C2S','rtt min',
+#	'rtt max','max seg size','min seg size','win max','win min','cwin max',
+#	'cwin min','initial cwin','rtx RTO','rtx FR','reordering','unnece rtx RTO','target'])
 
-print "*******"
-print df
+df = pd.read_csv(label_file, names=['Average rtt C2S', 'Average rtt S2C',
+	'max seg size1','min seg size1','win max1','win min1','win zero1','cwin max1',
+	'cwin min1','initial cwin1','rtx RTO1','rtx FR1','reordering1','net dup1','max seg size',
+	'in seg size','win max','win min','win zero','cwin max','cwin min','initial cwin','rtx RTO','rtx FR',
+	'eordering','net dup','num','target'])
+
+print("*******")
+#print df
 
 #extracting features
-features = ['Average rtt C2S', 'Average rtt S2C','max seg size1','min seg size1','win max1','win min1','win zero1','cwin max1',
-'cwin min1','initial cwin1','rtx RTO1','rtx FR1','reordering1','net dup1','max seg size','in seg size','win max','win min','win zero',
-'cwin max','cwin min','initial cwin','rtx RTO','rtx FR','eordering','net dup']
+features = ['Average rtt C2S', 'Average rtt S2C','max seg size1','min seg size1','win max1',
+'win min1','win zero1','cwin max1','cwin min1','initial cwin1','rtx RTO1','rtx FR1','reordering1','net dup1',
+'max seg size','in seg size','win max','win min','win zero','cwin max','cwin min','initial cwin','rtx RTO','rtx FR',
+'eordering','net dup']
 
 
+targets = ['Good', 'Bad']
+
+
+colors = ['g', 'r', 'b', 'black', 'lime', 'yellow', 'cyan', 'coral']
 
 # Separating out the features
 x = df.loc[:, features].values
 # Separating out the target
 y = df.loc[:,['target']].values
+print(y)
 # Standardizing the features
 x = StandardScaler().fit_transform(x)
 
-print "1"
+
+pd.set_option('display.max_columns', None)
+
+
+
 
 pca = PCA(n_components=2)
 principalComponents = pca.fit_transform(x)
@@ -53,48 +78,55 @@ principalDf = pd.DataFrame(data = principalComponents
 
 finalDf = pd.concat([principalDf, df[['target']]], axis = 1)
 
+#printing feature values
+
+
 #plot PCA
 
 fig = plt.figure(figsize = (8,8))
 ax = fig.add_subplot(1,1,1) 
-ax.set_xlabel('Principal Component 1', fontsize = 10)
-ax.set_ylabel('Principal Component 2', fontsize = 10)
-ax.set_title('2 component PCA', fontsize = 15)
-targets = ['Normal', 'Loss1%', 'Loss2%', 'pDup1%', 'pDup5%','reord25-50%','reord50-50%','Loss3%']
+#ax = fig.add_subplot(111, projection='3d')
 
-colors = ['r', 'g', 'b', 'black', 'lime', 'yellow', 'cyan', 'coral']
+ax.set_xlabel('x component', fontsize = 10)
+ax.set_ylabel('y component', fontsize = 10)
+
+#ax.set_title('2 component PCA', fontsize = 15)
+#targets = ['-1', '1']
+
+
+#colors = ['r', 'g']
 for target, color in zip(targets,colors):
 	indicesToKeep = finalDf['target'] == target
 	ax.scatter(finalDf.loc[indicesToKeep, 'principal component 1']
-  	, finalDf.loc[indicesToKeep, 'principal component 2']
-  	, c = color
-  	, s = 50)
+		, finalDf.loc[indicesToKeep, 'principal component 2']
+		, c = color
+		, s = 50)
 ax.legend(targets)
 ax.grid()
 plt.show()
 
-print "explained_variance_"
+print("explained_variance_")
 print(pca.explained_variance_)  
-print "singular values"
+print("singular values")
 print(pca.singular_values_) 
 
-print "variance ratio"
+print("variance ratio")
 print(pca.explained_variance_ratio_)
 
-print "mean"
+print("mean")
 print(pca.mean_)
 
 
 
-print "PCA has been conducted, with 2 component as a parameter and we fit the data"
+#print "PCA has been conducted, with 2 component as a parameter and we fit the data"
 
-print "now veiw the new features, number of rows and 2 features" 
+#print "now view the new features, number of rows and 2 features" 
 print(principalComponents.shape)
 
-print "new feature data"
+#print "new feature data"
 print(principalComponents)
 
-print "columns"
+#print "columns"
 intermediary=pd.DataFrame(pca.components_, columns=list(features)).values
 print(intermediary.reshape(2,26).tolist())
 
@@ -130,7 +162,7 @@ x -= np.mean(x, axis=0)
 cov_matrix = np.dot(x.T, x) / n_samples
 for eigenvector in pca.components_:
     print(np.dot(eigenvector.T, np.dot(cov_matrix, eigenvector)))
-print "end"
+
 
 
 
